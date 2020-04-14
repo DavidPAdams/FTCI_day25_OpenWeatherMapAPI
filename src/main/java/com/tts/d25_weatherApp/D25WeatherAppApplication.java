b6f1d9146f6f3a0c8b4f378e5a2b2cc3ac5f1a8f
David Adams <david.riding.rails@gmail.com>
2020-04-13 17:56:43 -0700

    initial commit

---
+++ src/main/java/com/tts/d25_weatherApp/D25WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.tts.d25_weatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D25WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D25WeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/d25_weatherApp/D25WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.d25_weatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D25WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
