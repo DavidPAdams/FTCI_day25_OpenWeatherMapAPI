5e89a04ce3475b77a45746f8a4e1dea8a86d3f7b
David Adams <david.riding.rails@gmail.com>
2020-04-13 17:57:03 -0700

    add response.java

---
+++ src/main/java/com/tts/d25_weatherApp/request/Request.java
@@ -0,0 +1,25 @@
+package com.tts.d25_weatherApp.request;
+
+public class Request {
+  private String zipCode;
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  @Override
+  public String toString() {
+    return "Request [zipCode=" + zipCode + "]";
+  }
+
+  public Request(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public Request() {}
+  
+}
