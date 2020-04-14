47d883eaa4bdb3c41a1edfc6809a965d24c5fb87
David Adams <david.riding.rails@gmail.com>
2020-04-13 17:57:43 -0700

    add weather controller

---
+++ src/main/java/com/tts/d25_weatherApp/controller/WeatherController.java
@@ -0,0 +1,31 @@
+package com.tts.d25_weatherApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tts.d25_weatherApp.request.Request;
+import com.tts.d25_weatherApp.response.Response;
+import com.tts.d25_weatherApp.service.WeatherService;
+
+@Controller
+public class WeatherController {
+  @Autowired
+  private WeatherService weatherService;
+  
+  @GetMapping
+  public String getIndex(Model model) {
+    model.addAttribute("request", new Request());
+    return "index";
+  }
+  
+  @PostMapping
+  public String postIndex(Request request, Model model) {
+    Response response = weatherService.getForcast(request.getZipCode());
+    model.addAttribute("data", response);
+    return "index";
+  }
+  
+}
