94f5e7844a54acf233000aa6f61b283fc566f6ee
David Adams <david.riding.rails@gmail.com>
2020-05-11 16:00:34 -0700

    update weather service for search list

--- src/main/java/com/tts/d25_weatherApp/service/WeatherService.java
+++ src/main/java/com/tts/d25_weatherApp/service/WeatherService.java
@@ -1,5 +1,6 @@
 package com.tts.d25_weatherApp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,18 +21,23 @@ public class WeatherService {
   
   @Value("${api_key}")
   private String apiKey;
+  
+  public List<Zips> findAllByDate(){
+    return zipCodeRepository.findAllByOrderByCreatedAtDesc();
+  }
 
   public Response getForcast(String zipCode) {
     String url = "http://api.openweathermap.org/data/2.5/weather?zip="
         + zipCode + "&units=imperial&appid=" + apiKey;
     RestTemplate restTemplate = new RestTemplate();
     Response response = new Response();
-    List<Zips> zipCodeList = zipCodeRepository.findAll();
+    List<Zips> zipCodeList = findAll();
     try {
       response = restTemplate.getForObject(url, Response.class);
       Zips zip = new Zips();
       zip.setZipCode(zipCode);
       zipCodeList.add(zip);
+      save(zip);
     } 
     catch(HttpClientErrorException ex) {
       response.setName("error");
@@ -39,6 +45,26 @@ public class WeatherService {
     return response;
   }
   
+  public List<Zips> findAll() {
+    return zipCodeRepository.findAll();
+  }
+
+  public void save(Zips zip) {
+    zipCodeRepository.save(zip);
+  }
   
+  public List<Zips> getLastTenZips() {
+    List<Zips> zipCodeList = findAllByDate();
+    List<Zips> zipLastTen = new ArrayList<>();
+    zipLoop:
+      for(Zips zip : zipCodeList) {
+        if (zipLastTen.size() <= 10) {
+          zipLastTen.add(zip);
+        } else {
+          break zipLoop;
+        }
+      }
+    return zipLastTen;
+  }
   
 }
